0cea7db39b8f4e882e18deaa41fff001ba55947e
esteban93040 <espinosa.esteban@correounivalle.edu.co>
2025-07-25 22:12:54 -0500

    primer commit

---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+export const Home   = () => {
+  return (
+    <div className="home">
+      <h1 className='home-title'>Welcome to the Home Page</h1>
+      <p className='home-description'>This is the home page of our application.</p>
+    </div>
+  );
+}
+
+export default Home;
